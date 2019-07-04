581212afd17eab94fd62c68e6d7fda357a9d2cf3
Harry Crocker <harry.crocker8@gmail.com>
2019-07-04 18:23:20 +0100

    background transition added to tablet header

--- assets/js/sticky-scroll-style-home.js
+++ assets/js/sticky-scroll-style-home.js
@@ -1,32 +1,28 @@
+let mTabletHeader = document.getElementsByTagName("header");
+
 window.onscroll = function () {
     // Desktop
     if (document.documentElement.scrollTop > 0) {
-        if (!mDesktopNav[0].classList.contains("scroll-top"))
-            mDesktopNav[0].classList.add("scroll-top");
+        mDesktopNav[0].classList.add("scroll-top");
     } else if (document.documentElement.scrollTop === 0) {
-        if (mDesktopNav[0].classList.contains("scroll-top"))
-            mDesktopNav[0].classList.remove("scroll-top");
+        mDesktopNav[0].classList.remove("scroll-top");
     }
 
     // Tablet
     if (document.documentElement.scrollTop > 65) {
-        if (!mTabletNav[0].classList.contains("scroll-top"))
-            mTabletNav[0].classList.add("scroll-top");
+        mTabletNav[0].classList.add("scroll-top");
     } else if (document.documentElement.scrollTop < 65) {
-        if (mTabletNav[0].classList.contains("scroll-top"))
-            mTabletNav[0].classList.remove("scroll-top");
+        mTabletNav[0].classList.remove("scroll-top");
     }
 
     // Home
     if (document.documentElement.scrollTop > 180) {
-        if (!mDesktopNav[0].classList.contains("scroll-top-home"))
-            mDesktopNav[0].classList.add("scroll-top-home");
-        if (!mTabletNav[0].classList.contains("scroll-top-home"))
-            mTabletNav[0].classList.add("scroll-top-home");
+        mDesktopNav[0].classList.add("scroll-top-home");
+        mTabletNav[0].classList.add("scroll-top-home");
+        mTabletHeader[0].classList.add("scroll-top-home-header");
     } else if (document.documentElement.scrollTop < 180) {
-        if (mDesktopNav[0].classList.contains("scroll-top-home"))
-            mDesktopNav[0].classList.remove("scroll-top-home");
-        if (mTabletNav[0].classList.contains("scroll-top-home"))
-            mTabletNav[0].classList.remove("scroll-top-home");
+        mDesktopNav[0].classList.remove("scroll-top-home");
+        mTabletNav[0].classList.remove("scroll-top-home");
+        mTabletHeader[0].classList.remove("scroll-top-home-header");
     }
 };
